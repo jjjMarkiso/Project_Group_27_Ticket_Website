8c1a9232756a50b4513805d6e6d569ceb440d5a8
jjjMarkiso <166040506+jjjMarkiso@users.noreply.github.com>
2025-11-17 18:50:31 -0700

    Updated main page and added main page css

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,15 +1,30 @@
 import Header from './header_footer/Header.jsx';
 import Footer from './header_footer/Footer.jsx';
+import styles from './main.module.css';
 
 function MainPage() {
+
+    const items = [{ itemName: "Item 1", itemId: "1"},
+        { itemName: "Item 2", itemId: "2"},
+        { itemName: "Item 3", itemId: "3"},
+        { itemName: "Item 4", itemId: "4"},
+        { itemName: "Item 5", itemId: "5"},
+        { itemName: "Item 6", itemId: "6"},
+        { itemName: "Item 7", itemId: "7"},
+        { itemName: "Item 8", itemId: "7"},
+        { itemName: "Item 9", itemId: "7"},]
+
     return (
         <>
-            <Header />
+            <Header />            
 
-            <main>
-                <h1>Welcome to the Main Page</h1>
-                <p>This is the content of your website.</p>
-            </main>
+            <div className={styles.container}>
+                {
+                    items.map((val) => {
+                        return <div className={styles.item}>{val.itemName}</div>
+                    })
+                }
+            </div>
 
             <Footer />
         </>
